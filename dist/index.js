318fc7cb7089e3f42cb6794d9b55da769719921f
Carlo Nicora <carlo.nicora@gmail.com>
2024-05-16 11:36:28 +0200

    fix: correct export

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.bufferToUuid = exports.uuidToBuffer = exports.DataValidator = exports.Router = exports.JsonApiBuilder = exports.OptionalJwtAuthGuard = exports.JwtStrategy = exports.JwtAuthGuard = exports.AuthModule = exports.JsonApiNavigator = exports.Imgix = void 0;
+exports.JsonApiPipe = exports.bufferToUuid = exports.uuidToBuffer = exports.DataValidator = exports.Router = exports.JsonApiBuilder = exports.OptionalJwtAuthGuard = exports.JwtStrategy = exports.JwtAuthGuard = exports.AuthModule = exports.JsonApiNavigator = exports.Imgix = void 0;
 var Imgix_1 = require("./imgix/Imgix");
 Object.defineProperty(exports, "Imgix", { enumerable: true, get: function () { return Imgix_1.Imgix; } });
 var JsonApiNavigator_1 = require("./jsonApi/JsonApiNavigator");
@@ -36,4 +36,6 @@ function bufferToUuid(buffer) {
     return uuid;
 }
 exports.bufferToUuid = bufferToUuid;
+var JsonApiPipe_1 = require("./jsonApi/JsonApiPipe");
+Object.defineProperty(exports, "JsonApiPipe", { enumerable: true, get: function () { return JsonApiPipe_1.JsonApiPipe; } });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,30 @@ export { JwtAuthGuard } from "./auth/jwt-auth.guard";
 export { JwtStrategy } from "./auth/jwt.strategy";
 export { OptionalJwtAuthGuard } from "./auth/optional-jwt-auth.guard";
 export { JsonApiBuilder } from "./jsonApi/JsonApiBuilder";
-export { JsonApiDataInterface, transformFunction } from "./jsonApi/interfaces/JsonApiDataInterface";
+export {
+  JsonApiDataInterface,
+  transformFunction,
+} from "./jsonApi/interfaces/JsonApiDataInterface";
 export { Router } from "./routing/Router";
 export { DataValidator } from "./validator/DataValidator";
 
 export function uuidToBuffer(uuid: string): Buffer {
-	const hex = uuid.replace(/-/g, "");
-	return Buffer.from(hex, "hex");
+  const hex = uuid.replace(/-/g, "");
+  return Buffer.from(hex, "hex");
 }
 
 export function bufferToUuid(buffer: Buffer): string {
-	const hex = buffer.toString("hex");
-	const uuid = [
-		hex.substring(0, 8),
-		hex.substring(8, 12),
-		hex.substring(12, 16),
-		hex.substring(16, 20),
-		hex.substring(20, 32),
-	].join("-");
+  const hex = buffer.toString("hex");
+  const uuid = [
+    hex.substring(0, 8),
+    hex.substring(8, 12),
+    hex.substring(12, 16),
+    hex.substring(16, 20),
+    hex.substring(20, 32),
+  ].join("-");
 
-	return uuid;
+  return uuid;
 }
+
+export { JsonApiPipe } from "./jsonApi/JsonApiPipe";
+export { JsonApiBuilderInterface } from "./jsonApi/interfaces/JsonApiBuilderInterface";
