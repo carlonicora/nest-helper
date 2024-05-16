dd9f3009c030b0b4bde0fa0ce7612a0bc6d8513d
Carlo Nicora <carlo.nicora@gmail.com>
2024-05-16 09:26:53 +0200

    feat: update to JsonApiBuilderInterface

--- src/jsonApi/interfaces/JsonApiDataInterface.ts
+++ src/jsonApi/interfaces/JsonApiDataInterface.ts
@@ -1,26 +1,31 @@
+import { JsonApiBuilderInterface } from "./JsonApiBuilderInterface";
+
 export type transformFunction<T> = (data: T) => any;
 
 export interface JsonApiDataInterface {
-	type: string;
-	id: string | transformFunction<any>;
-	attributes: {
-		[key: string]: any;
-	};
-	meta?: {
-		[key: string]: any;
-	};
-	links?: {
-		self: transformFunction<any>;
-	};
-	relationships?: {
-		[key: string]: {
-			resourceIdentifier?: { type: string; id: string | transformFunction<any> };
-			data?: JsonApiDataInterface;
-			included?: boolean;
-			name?: string;
-			links?: {
-				related?: transformFunction<any>;
-			};
-		};
-	};
+  type: string;
+  id: string | transformFunction<any>;
+  attributes: {
+    [key: string]: any;
+  };
+  meta?: {
+    [key: string]: any;
+  };
+  links?: {
+    self: transformFunction<any>;
+  };
+  relationships?: {
+    [key: string]: {
+      resourceIdentifier?: {
+        type: string;
+        id: string | transformFunction<any>;
+      };
+      data?: JsonApiBuilderInterface;
+      included?: boolean;
+      name?: string;
+      links?: {
+        related?: transformFunction<any>;
+      };
+    };
+  };
 }
