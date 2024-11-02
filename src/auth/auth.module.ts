279bb28578b032a2002c3c994156497c559af1fb
Carlo Nicora <carlo.nicora@gmail.com>
2024-11-02 17:19:07 +0100

    refactor: remove JWT

--- src/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { JwtStrategy } from "./jwt.strategy";
-
-@Module({
-	providers: [JwtStrategy],
-	exports: [JwtStrategy],
-})
-export class AuthModule {}
